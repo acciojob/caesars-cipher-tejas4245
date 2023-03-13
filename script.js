1126b9987d2728ac16702d162af2996a527b4589
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-13 17:34:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your Script here.
+function rot13(str) {
+	let decodedStr="";
+	for(let i=0;i<str.length;i++){
+		let char =str[i];
+		if (char.match(/[A-Z])){
+					   let charCode =str.charCodeAt(i)-65;
+		charCode=(charCode+13)%26;
+		charCode=(charCode+13)%26;
+		char=string.fromCharCode(charCode+65);
+	}
+	decodeStr+=char;
+}
+return decodeStr;
+	
+}
